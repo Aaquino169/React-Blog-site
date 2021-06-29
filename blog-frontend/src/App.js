64d2780468f08825e65ad589cfaaec9d7e5cb777
alexander_aquino <nanito169@gmail.com>
2021-06-29 14:10:47 -0400

    added css and created header and home component

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,8 +1,10 @@
 import NavBar from "./navbar/NavBar";
+import Home from "./pages/home/Home"
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Home/>
     </div>
   );
 }
--- blog-frontend/src/navbar/NavBar.jsx
+++ blog-frontend/src/navbar/NavBar.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import "./navbar.css"
 
+import logo from "../img/20201216_170947.jpg"
+
 export default function NavBar() {
+    
+    
+
     return (
         <div className="nav">
-            Nav Bar
+            <div className="navLeft">
+            <i className="navIcon fab fa-twitter-square"></i>
+            <i className="navIcon fab fa-facebook-square"></i>
+            <i className="navIcon fab fa-instagram-square"></i>
+            <i className="navIcon fab fa-pinterest-square"></i>
+            </div>
+            <div className="navCenter">
+                <ul className="navList">
+                    <li className="navListItem">HOME</li>
+                    <li className="navListItem">ABOUT</li>
+                    <li className="navListItem">CONTACT</li>
+                    <li className="navListItem">WRITE</li>
+                    <li className="navListItem">LOGOUT</li>
+                </ul>
+            </div>
+            <div className="navRight">
+                <img 
+                    className="navImg" 
+                    src="https://cdn.dribbble.com/users/1577045/screenshots/4914645/dribble_pic.png" 
+                    alt="" 
+                />
+                <i className="navSearchIcon fas fa-search"></i>
+            </div>
         </div>
     )
 }
