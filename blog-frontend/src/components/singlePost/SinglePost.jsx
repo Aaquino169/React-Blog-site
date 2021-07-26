6bd2c5c8e4d62b0b4c2c553db8bbec6d02a4f8e4
alexander_aquino <nanito169@gmail.com>
2021-07-26 15:12:31 -0400

    created single post component added css

--- blog-frontend/src/components/singlePost/SinglePost.jsx
+++ blog-frontend/src/components/singlePost/SinglePost.jsx
@@ -1,11 +1,25 @@
 import "./singlePost.css"
-import
+import img from "../img/postImg.jpg"
 
 export default function SinglePost() {
     return (
         <div className="singlePost">
             <div className="singlePostContainer">
-                <img src="" alt="" />
+                <img src={img} alt="" className="singlePostImg"/>
+                <h1 className="singlePostTitle">
+                    Lorem ipsum dolor sit amet consectetur.
+                    <div className="singlePostEdit">
+                        <i className="singlePostIcon fas fa-edit"></i>
+                        <i className="singlePostIcon fas fa-trash-alt"></i>
+                    </div>
+                </h1>
+                <div className="singlePostInfo">
+                    <span className="singlePostAuthor">
+                        Author: <b>Alex</b>
+                    </span>
+                    <span className="singlePostDate">1 hour ago</span>
+                </div>
+                <p className="singlePostDescription">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis mollitia culpa laudantium ex quasi illo itaque at atque reiciendis, placeat libero cupiditate, similique, dolor provident minima tenetur praesentium error repellendus!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis mollitia culpa laudantium ex quasi illo itaque at atque reiciendis, placeat libero cupiditate, similique, dolor provident minima tenetur praesentium error repellendus!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis mollitia culpa laudantium ex quasi illo itaque at atque reiciendis, placeat libero cupiditate, similique, dolor provident minima tenetur praesentium error repellendus!</p>
             </div>
         </div>
     )
---
+++ blog-frontend/src/pages/write/Write.jsx
@@ -0,0 +1,13 @@
+import './write.css'
+
+export default function Write() {
+    return (
+        <div className="write">
+            <div className="writeForm">
+                <div className="writeFormGroup">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
